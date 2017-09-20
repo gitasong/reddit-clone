1e18042079fdeb7336b13f34a7640c4861f9b692
Guest User <nicole.freed@gmail.com>
2017-09-20 16:17:16 -0700

    import Header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from ".Header";
 import Feed from "./Feed";
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Feed></Feed>
     </div>
   );
