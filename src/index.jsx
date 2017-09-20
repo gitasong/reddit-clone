f3aaaac576f6874b567bf8bb3bbd3fe31a506712
Guest User <nicole.freed@gmail.com>
2017-09-20 15:48:18 -0700

    add boilerplate to index.jsx w/redux stuff commented out

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
+// import { createStore } from 'redux';
+// import reducer from './reducers/ticket-list-reducer';
+// import { Provider } from 'react-redux';
+
+import App from "./components/App";
+
+// const store = createStore(reducer);
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById("react-app-root")
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept("./components/App", () => {
+    render(App);
+  });
+}
