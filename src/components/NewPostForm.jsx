4e78e77ef9e1a8df9713e4f50edd45a5b970ff08
Guest User <nicole.freed@gmail.com>
2017-09-20 16:41:37 -0700

    convert NewTicketForm to stateful class-based component

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 
-function NewPostForm() {
-  return (
-    <form>
-      <input
-          ref="_title"
-          type="text"
-          id="title"
-          placeholder="Post Title"/>
-          <br/>
-          <input
-          ref="_author"
-          type="text"
-          id="author"
-          placeholder="Author Alias"/>
-          <br/>
-          <textarea
-          ref="_content"
-          id="content"
-          placeholder="Your scintillating content goes here."/>
-          <br/>
-        <button type="submit">Post</button>
-    </form>
-  );
+class NewPostForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+            ref="_title"
+            type="text"
+            id="title"
+            placeholder="Post Title"/>
+            <br/>
+            <input
+            ref="_author"
+            type="text"
+            id="author"
+            placeholder="Author Alias"/>
+            <br/>
+            <textarea
+            ref="_content"
+            id="content"
+            placeholder="Your scintillating content goes here."/>
+            <br/>
+          <button type="submit">Post</button>
+      </form>
+    );
+  }
 }
 
 export default NewPostForm;
