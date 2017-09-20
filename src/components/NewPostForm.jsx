2b69e288a65dc483bea5e4200db11a42c0266464
Guest User <nicole.freed@gmail.com>
2017-09-20 16:27:36 -0700

    add form input fields w/refs

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -3,7 +3,21 @@ import React from "react";
 function NewPostForm() {
   return (
     <form>
-
+      <input
+          ref="_title"
+          type="text"
+          id="title"
+          placeholder="Post Title"/>
+          <input
+          ref="_author"
+          type="text"
+          id="author"
+          placeholder="Author Alias"/>
+          <textarea
+          ref="_content"
+          id="content"
+          placeholder="Your scintillating content goes here."/>
+        <button type="submit">Post</button>
     </form>
   );
 }
