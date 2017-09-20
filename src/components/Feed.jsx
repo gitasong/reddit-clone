ab88e55cc5e756dfc22d2f911e5d65daaf9ba36d
Guest User <nicole.freed@gmail.com>
2017-09-20 16:23:32 -0700

    import NewPostForm into Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import PostList from "./PostList";
+import NewPostForm from "./NewPostForm";
 
 function Feed() {
   return (
     <div>
+      <NewPostForm></NewPostForm>
       <PostList></PostList>
     </div>
   );
