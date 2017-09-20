f8eaabfc5db5ceb840f9b05105f8d88e2ccd4ced
Guest User <nicole.freed@gmail.com>
2017-09-20 16:16:20 -0700

    create Header component w/logo and title

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <img src="https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/012011/reddit-logo2.png?itok=7RB395dU"/>
+      <h1>Reddit Clone</h1>
+    </div>
+  );
+}
+
+export default Header;
