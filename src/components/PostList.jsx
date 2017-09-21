06ecdafe82ac2cd055c3a3971234fa3b4c8d17fc
Guest User <nicole.freed@gmail.com>
2017-09-20 17:47:56 -0700

    import and declare PropTypes; add props to Post component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Post from "./Post";
+import PropTypes from "prop-types";
 
-function PostList() {
+function PostList(props) {
   return (
     <div>
-      <Post></Post>
+      <hr/>
+      {props.postList.map((post, index) =>
+        <Post title={post.title}
+        timestamp={post.timestamp}
+        author={post.author}
+        content={post.content}
+        votes={post.votes}
+        key={index}></Post>
+      )}
     </div>
   );
+}
+
+PostList.PropTypes = {
+  postList: PropTypes.array
 }
 
 export default PostList;
