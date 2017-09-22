d5315bee25b56ea8787541fa15f10510c3bbb10a
Nicole Freed <nicole.freed@gmail.com>
2017-09-22 09:27:14 -0700

    import post list reducer

--- __tests__/reducers/post-list-reducer.test.js
+++ __tests__/reducers/post-list-reducer.test.js
@@ -1,3 +1,5 @@
+import postList from './../../src/reducers/post-list-reducer';
+
 describe("Post list reducer", () => {
   test('temporary dummy test: two plus two is four', () => {
     expect(2 + 2).toBe(4);
