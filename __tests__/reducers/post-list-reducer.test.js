3cf1de106d8473e7084f18348ac81f1de5a98edc
Nicole Freed <nicole.freed@gmail.com>
2017-09-22 09:30:14 -0700

    add test to return equivalent state if no action type recognized

--- __tests__/reducers/post-list-reducer.test.js
+++ __tests__/reducers/post-list-reducer.test.js
@@ -1,7 +1,7 @@
 import postList from './../../src/reducers/post-list-reducer';
 
 describe("Post list reducer", () => {
-  test('temporary dummy test: two plus two is four', () => {
-    expect(2 + 2).toBe(4);
+  test('should return equivalent state if no action is recognized', () => {
+    expect(postList([], { type: null})).toEqual([]);
   });
 });
