d61532ac24f60a041fa9823f012ab7d41c4250f7
Nicole Freed <nicole.freed@gmail.com>
2017-09-22 09:15:03 -0700

    convert Feed to class-based component to hold state

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import PostList from "./PostList";
 import NewPostForm from "./NewPostForm";
 
-function Feed() {
-  return (
-    <div>
-      <NewPostForm></NewPostForm>
-      <PostList></PostList>
-    </div>
-  );
+class Feed extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <NewPostForm></NewPostForm>
+        <PostList></PostList>
+      </div>
+    );
+  }
 }
 
 export default Feed;
