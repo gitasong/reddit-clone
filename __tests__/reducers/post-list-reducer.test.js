5712f81a4f93d05e33aa95ade0ac58562aa9fc1c
Nicole Freed <nicole.freed@gmail.com>
2017-09-22 09:43:41 -0700

    add dummy test to check for successful constants importing

--- __tests__/reducers/post-list-reducer.test.js
+++ __tests__/reducers/post-list-reducer.test.js
@@ -4,4 +4,9 @@ describe("Post list reducer", () => {
   test('should return equivalent state if no action is recognized', () => {
     expect(postList([], { type: null})).toEqual([]);
   });
+
+  test('imported value should match action type string', () => {
+    expect(constants.ADD_POST).toEqual('action ADD_POST has been successfully imported');
+  });
+
 });
