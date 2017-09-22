a380102eccda678fc324905e751d5710d1703140
Nicole Freed <nicole.freed@gmail.com>
2017-09-22 09:48:05 -0700

    import constants file

--- __tests__/reducers/post-list-reducer.test.js
+++ __tests__/reducers/post-list-reducer.test.js
@@ -1,4 +1,5 @@
 import postList from './../../src/reducers/post-list-reducer';
+import constants from './../../src/constants';
 
 describe("Post list reducer", () => {
   test('should return equivalent state if no action is recognized', () => {
