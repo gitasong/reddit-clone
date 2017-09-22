c11a4fb10b799f83d8ed5502f8174f6da5ee9af2
Nicole Freed <nicole.freed@gmail.com>
2017-09-22 09:57:24 -0700

    define action, add dummy postInfo

--- __tests__/reducers/post-list-reducer.test.js
+++ __tests__/reducers/post-list-reducer.test.js
@@ -2,6 +2,16 @@ import postList from './../../src/reducers/post-list-reducer';
 import constants from './../../src/constants';
 
 describe("Post list reducer", () => {
+  let action;
+  const postInfo = {
+    title: "Redux is badass",
+    timestamp: 4500000000000,
+    author: "superWebDev",
+    content: "Redux makes changing state a snap. You should use it!"
+    votes: 7,
+    id: 0
+  };
+
   test('should return equivalent state if no action is recognized', () => {
     expect(postList([], { type: null})).toEqual([]);
   });
