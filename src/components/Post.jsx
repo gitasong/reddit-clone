5b5bf9fc4f58393e2e1156c76c78361b7e9cb20f
Nicole Freed <nicole.freed@gmail.com>
2017-09-22 10:49:05 -0700

    add Post display details

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,11 @@ import PropTypes from "prop-types";
 function Post() {
   return (
     <div>
-      Post stuff goes here.
+      <h3>{props.title}</h3>
+      <small>{props.timestamp}</small>
+      <h5><em>{props.author}</em></h5>
+      <p>{props.content}</p>
+      <h5>Upvotes: {props.votes}</h5>
     </div>
   );
 }
