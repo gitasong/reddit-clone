d3acb9f5f6109119c2ebde7cb2974153c786408e
Nicole Freed <nicole.freed@gmail.com>
2017-09-22 10:27:27 -0700

    create submitForm method w/action & dispatch

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -8,6 +8,24 @@ class NewPostForm extends React.Component {
   constructor(props) {
     console.log(props);
     super(props);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  submitForm(event) {
+    event.preventDefault();
+    const { _title, _author, _content } = this.refs;
+    const { dispatch } = this.props;
+    const action = {
+      type: c.ADD_POST,
+      id: id,
+      title: _title.val,
+      timestamp: new Date().getTime(),
+      author: _author.val,
+      content: _content.val,
+      votes: 0,
+    }
+    console.log(action.id);
+    dispatch(action);
   }
 
   render() {
